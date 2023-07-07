82f4267151c0fb28ed50c7471bd60b481798ebcb
Somesh Chavhan <someshchavhan904@gmail.com>
2023-07-07 20:23:36 +0530

    Footer complete and Project finished

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,13 @@
-import { Box, Button, Grid, Link, Paper, Stack, Typography } from '@mui/material'
+import { Button, FormControl, Grid, IconButton, Link, OutlinedInput, Paper, Stack, Typography } from '@mui/material'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import PinterestIcon from '@mui/icons-material/Pinterest';
+import MpIcon from '@mui/icons-material/Mp';
+import MarkunreadIcon from '@mui/icons-material/Markunread';
+import CallRoundedIcon from '@mui/icons-material/CallRounded';
 
 import tree1 from '../../images/tree1.png'
 import tree2 from '../../images/tree2.png'
@@ -152,7 +161,98 @@ const Footer = () => {
                     </Grid>
                     {/* 4th Grid component Services end */}
                 </Grid>
-
+                <Grid container sx={{
+                    position: 'relative',
+                    left: '4rem',
+                    top: '2rem'
+                }}>
+                    {/* Logo Container */}
+                    <Grid item xs={2}>
+                        <img src={logo} alt="..." />
+                    </Grid>
+                    {/* Logo Container Ended */}
+                    <Grid item xs={3}>
+                        <Stack direction='column' >
+                            {/* Input Field */}
+                            <Stack direction='row' justifyContent='center' p={1} sx={{
+                                background: 'grey',
+                                borderTopLeftRadius: '10px',
+                                borderBottomRightRadius: '10px'
+                            }} >
+                                <Typography variant='h6' component='span' color={'#fff'}>
+                                    Subscribe for Trading &nbsp;
+                                </Typography>
+                                <Typography variant='h6' component='span' color={'#FED11F'}>
+                                    NewsLetter
+                                </Typography>
+                            </Stack>
+                            <Stack direction='row' my={1} spacing={1}>
+                                <FormControl>
+                                    <OutlinedInput placeholder='email' color='info' sx={{
+                                        background: '#fff'
+                                    }} />
+                                </FormControl>
+                                <Button variant='contained' color='info'>
+                                    subscribe
+                                </Button>
+                            </Stack>
+                            {/* INput field End */}
+                        </Stack>
+                    </Grid>
+                    {/* Social Media buttons */}
+                    <Grid item xs={3}>
+                        <Stack direction='column'>
+                            <Stack direction='row' spacing={0} justifyContent='center'>
+                                <IconButton aria-label='facebook' color='info' size='large'>
+                                    <FacebookIcon fontSize='large' />
+                                </IconButton>
+                                <IconButton aria-label='instagram' color='info' size='large'>
+                                    <InstagramIcon fontSize='large' />
+                                </IconButton>
+                                <IconButton aria-label='linkedIn' color='info' size='large'>
+                                    <LinkedInIcon fontSize='large' />
+                                </IconButton>
+                                <IconButton aria-label='youtube' color='info' size='large'>
+                                    <YouTubeIcon fontSize='large' />
+                                </IconButton>
+                            </Stack>
+                            <Stack direction='row' spacing={0} justifyContent='center'>
+                                <IconButton aria-label='twitter' color='info' size='large'>
+                                    <TwitterIcon fontSize='large' />
+                                </IconButton>
+                                <IconButton aria-label='pintrest' color='info' size='large'>
+                                    <PinterestIcon fontSize='large' />
+                                </IconButton>
+                                <IconButton aria-label='monster' color='info' size='large'>
+                                    <MpIcon fontSize='large' />
+                                </IconButton>
+                            </Stack>
+                        </Stack>
+                    </Grid>
+                    {/* Social Media buttons Ended */}
+                    {/* Contact Details */}
+                    <Grid item xs={3}>
+                        <Stack direction='column' spacing={2}>
+                            <Stack direction='row'>
+                                <IconButton aria-label='mail' color='info' size='large'>
+                                    <MarkunreadIcon fontSize='large' />
+                                </IconButton>
+                                <Link variant='h5' color={'#FED11F'} underline='hover' my={2}>
+                                    team@gbjbuzz.com
+                                </Link>
+                            </Stack>
+                            <Stack direction='row'>
+                                <IconButton aria-label='mail' color='info' size='large'>
+                                    <CallRoundedIcon fontSize='large' />
+                                </IconButton>
+                                <Link variant='h5' color={'#FED11F'} underline='hover' my={2}>
+                                    +91 - 7020107390
+                                </Link>
+                            </Stack>
+                        </Stack>
+                    </Grid>
+                    {/* Contact Details Ended */}
+                </Grid>
             </Paper>
 
         </>
