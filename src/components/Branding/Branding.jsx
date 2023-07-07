7d777901e9b665315429c8ff1d60bf2bb22a219b
Somesh Chavhan <someshchavhan904@gmail.com>
2023-07-07 15:10:29 +0530

    style externally imported

--- src/components/Branding/Branding.jsx
+++ src/components/Branding/Branding.jsx
@@ -2,44 +2,37 @@ import { Paper, Stack, Typography } from '@mui/material'
 import TaskAltIcon from '@mui/icons-material/TaskAlt';
 
 import servicesLogo from '../../images/servicesLogo.png'
+import { styles } from './styles';
 const Branding = () => {
     return (
         // Branding Contianer started
-        <Paper sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '100vh'
-        }} id='Branding Container'>
+        <Paper sx={styles.container} id='Branding Container'>
             <Stack direction='column' spacing={2}>
                 {/* Moto Text */}
                 <Stack direction='row' spacing={2}>
                     <TaskAltIcon color='info' sx={{
-                        fontSize: 100
-                    }} />
-                    <Typography variant='h1' color='text.primary' sx={{
-                        fontWeight: 900,
                         fontSize: 120
-                    }}>
+                    }} />
+                    <Typography variant='h1' color='text.primary' sx={styles.primartFont}>
                         Exclusive
                     </Typography>
                     <Stack direction='column' sx={{
                         position: 'relative',
                         top: '1rem'
                     }}>
-                        <Typography variant='h3' color='text.primary' sx={{ fontWeight: 900 }}>
+                        <Typography variant='h3' color='text.primary' sx={styles.font900}>
                             Premium
                         </Typography>
-                        <Typography variant='h3' color='text.primary' sx={{ fontWeight: 900 }}>
+                        <Typography variant='h3' color='text.primary' sx={styles.font900}>
                             Support
                         </Typography>
                     </Stack>
                 </Stack>
                 <Stack direction='column' spacing={2} >
-                    <Typography color='text.primary' variant='h3' sx={{ fontWeight: 600 }}>
+                    <Typography color='text.primary' variant='h3' sx={styles.font600}>
                         Skyrocket Your Business Success: Unleash
                     </Typography>
-                    <Typography color='text.primary' variant='h3' sx={{ fontWeight: 600 }}>
+                    <Typography color='text.primary' variant='h3' sx={styles.font600}>
                         The Power of GBJ Premium Support With
                     </Typography>
                     {/* Moto Text Ended */}
---
+++ src/components/Branding/styles.js
@@ -0,0 +1,14 @@
+export const styles = {
+    container: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh'
+    },
+    primartFont: {
+        fontWeight: 900,
+        fontSize: 120
+    },
+    font900: { fontWeight: 900 },
+    font600: { fontWeight: 600 },
+}
